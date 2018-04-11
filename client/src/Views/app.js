d0a5e1c332089db2ee3b79d8934cd089bf2e8158
Lewis Fong <fongl@bu.edu>
2018-04-11 13:57:44 -0700

    first commit

---
+++ client/src/Views/app.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import Organization from './Organization';
+import './app.scss';
+export default props =>
+<BrowserRouter>
+  <div className="app">
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/organization">Organizations</Link></li>
+    </ul>
+
+    <hr/>
+
+    <Route exact path="/" component={Home}/>
+    <Route path="/organization" component={Organization}/>
+    <Route path="/team" component={Home}/>
+  </div>
+</BrowserRouter>
